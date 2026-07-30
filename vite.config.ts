cbf4c82cd580d0b8f4107bb8c106e29831431200
duds-maia <edumaiars24@gmail.com>
2026-07-30 15:42:53 -0300

    arrumando o vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
-const repoName = 'app_clima.github.io';
+const repoName = '/app_clima/';
 
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/',
